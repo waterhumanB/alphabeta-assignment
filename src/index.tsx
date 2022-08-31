95199064152f95ebe669503de99d233578624873
waterhumanB <dpfflsk45@gmail.com>
2022-09-01 00:53:32 +0900

    feat: 배포 흰화면 오류 HashRouter로 교체

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 import App from './routes'
 import GlobalStyle from './styles'
@@ -8,9 +8,9 @@ import GlobalStyle from './styles'
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <GlobalStyle />
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
